bafe8cbd7036ea788e7e00aebaecdcc967abb076
Syed Abdullah <syed.abdullah@xoop.pk>
2019-02-08 16:51:51 +0500

    Base template for angular powered dasbhoard apps. Included bootstrap, router, templates.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddBookingComponent } from './add-booking/add-booking.component';
+import { BookingsComponent } from './bookings/bookings.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'new/booking', component: AddBookingComponent },
+  { path: 'list/bookings', component: BookingsComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AddBookingComponent,
+    BookingsComponent,
+    PageNotFoundComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
+    NgbModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
